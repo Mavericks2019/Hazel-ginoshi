1ce51d52d634f5e6a25581dd37228188d862adc9
Mavericks2019 <1015673037@qq.com>
2026-03-18 12:39:38 +0800

    Log system

--- src/Hazel/EntryPoint.h
+++ src/Hazel/EntryPoint.h
@@ -4,8 +4,13 @@
 
 extern Hazel::Application* Hazel::CreateApplication();
 
-int main(int argc, char** argv) {
-	printf("Hazel Engine");
+int main(int argc, char** argv) 
+{
+	Hazel::Log::Init();
+	HZ_CORE_WARN("test log!");
+	int a = 10;
+	HZ_CLIENT_INFO("Hello var = {0}!", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
