8a1cb1deba67dd9584352e4db00b215f9b176cff
Mavericks2019 <1015673037@qq.com>
2026-03-25 13:30:00 +0800

    first triangle

--- Sandox/src/SandoxApp.cpp
+++ Sandox/src/SandoxApp.cpp
@@ -40,8 +40,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		//ImGui::Begin("Test");
-		//ImGui::Text("Hello World");
+		ImGui::Begin("Test");
+		ImGui::Text("Hello World");
+		ImGui::End();
 	}
 
 	void OnEvent(Hazel::Event& event) override
