33fd80260baec5c6c2cf1bd55884fab2e6440983
Mavericks2019 <1015673037@qq.com>
2026-03-27 01:35:04 +0800

    abstract-buffer: Refactor buffer abstraction layer
    - Update Buffer.h with abstract buffer interface
    - Implement OpenGL buffer implementations
    - Update Application.cpp with buffer usage
    - Update imgui configuration

--- src/Platform/OpenGL/OpenGLBuffer.h
+++ src/Platform/OpenGL/OpenGLBuffer.h
@@ -13,11 +13,16 @@ namespace Hazel {
 		virtual void Bind() const;
 		virtual void Unbind() const;
 
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
 		virtual void SetData(const void* data, uint32_t size) override;
 
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
+
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
