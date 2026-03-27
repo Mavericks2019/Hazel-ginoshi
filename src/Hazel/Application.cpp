f617de60865d80ec220fc2d5bfa27b93c1d56ede
Mavericks2019 <1015673037@qq.com>
2026-03-27 11:44:24 +0800

    vertex array and square

--- src/Hazel/Application.cpp
+++ src/Hazel/Application.cpp
@@ -42,8 +42,7 @@ namespace Hazel {
 		m_ImGuiLayer = new ImGuiLayer;
 		PushOverlay(m_ImGuiLayer);
 		
-		glGenVertexArrays(1, &m_VertexArray);
-		glBindVertexArray(m_VertexArray);
+		m_VertexArray.reset(VertexArray::Create());
 
 		float vertices[3 * 7] = {
 			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
@@ -51,36 +50,43 @@ namespace Hazel {
 			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 
-		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		std::shared_ptr<VertexBuffer> vertexBuffer;
 
+		vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+
+		BufferLayout layout =
 		{
-			BufferLayout layout =
-			{
-				{ ShaderDataType::Float3, "a_Position"},
-				{ ShaderDataType::Float4, "a_Color" },
-			};
-
-			m_VertexBuffer->SetLayout(layout);
-		}
-		const auto layout = m_VertexBuffer->GetLayout();
-		uint32_t index = 0;
-
-		for (const auto& element : layout)
-		{
-			glEnableVertexAttribArray(index);
-			glVertexAttribPointer(index, 
-								element.GetComponentCount(), 
-								ShaderDataTypeToOpenGLBaseType(element.Type), 
-								element.Normalized? GL_TRUE: GL_FALSE, 
-								layout.GetStride(),
-								(const void*)element.Offset);
-			index++;
-		}
-
+			{ ShaderDataType::Float3, "a_Position"},
+			{ ShaderDataType::Float4, "a_Color" },
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
 
 		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<IndexBuffer> indexBuffer;
+		indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
 
-		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_SquareVA.reset(VertexArray::Create());
+
+		float suqarevertices[3 * 4] = {
+			-0.5f, -0.75f, 0.0f,
+			 0.5f, -0.75f, 0.0f,
+			 0.5f,  0.75f, 0.0f,
+			 -0.5f,  0.75f, 0.0f,
+		};
+
+		std::shared_ptr<VertexBuffer> m_SquareVB(VertexBuffer::Create(suqarevertices, sizeof(suqarevertices)));
+		
+		m_SquareVB->SetLayout({
+			{ ShaderDataType::Float3, "a_Position"}
+		});
+		m_SquareVA->AddVertexBuffer(m_SquareVB);
+
+
+		uint32_t squareindices[6] = { 0, 1, 2 ,2, 3, 0};
+		std::shared_ptr<IndexBuffer> m_SquareIB(IndexBuffer::Create(squareindices, sizeof(squareindices) / sizeof(uint32_t)));		
+		m_SquareVA->SetIndexBuffer(m_SquareIB);
 
 		std::string vertexSrc = R"(
 			#version 330 core
@@ -112,6 +118,34 @@ namespace Hazel {
 		)";
 
 		m_Shader.reset(new Shader(vertexSrc, fragSrc));
+
+		std::string vertexSrc2 = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;  
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);
+			};
+		)";
+
+		std::string fragSrc2 = R"(
+			#version 330 core
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_Shader2.reset(new Shader(vertexSrc2, fragSrc2));
 	}
 
 	Application::~Application()
@@ -162,9 +196,13 @@ namespace Hazel {
 			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			m_Shader2->Bind();
+			m_SquareVA->Bind();
+			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+
 			m_Shader->Bind();
-			glBindVertexArray(m_VertexArray);
-			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
+			m_VertexArray->Bind();
+			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 
 			for (Layer* layer : m_LayerStack)
 			{
--- src/Hazel/Application.h
+++ src/Hazel/Application.h
@@ -9,6 +9,7 @@
 
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
 
 namespace Hazel {
 
@@ -33,11 +34,10 @@ namespace Hazel {
 		bool OnWindowClose(WindowCloseEvent& e);
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<Shader> m_Shader2;
+		std::shared_ptr<VertexArray> m_VertexArray;
+		std::shared_ptr<VertexArray> m_SquareVA;
 
 	private:
 		static Application* s_Instance;
