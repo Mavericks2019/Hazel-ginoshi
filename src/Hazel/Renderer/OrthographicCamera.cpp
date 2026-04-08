ada300974c87d0191f2c6bfda429089c51c72748
Mavericks2019 <1015673037@qq.com>
2026-04-09 00:24:33 +0800

    48

--- src/Hazel/Renderer/OrthographicCamera.cpp
+++ src/Hazel/Renderer/OrthographicCamera.cpp
@@ -25,6 +25,7 @@ namespace Hazel {
 	{
 		HZ_PROFILE_FUNCTION();
 
+
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
 			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
 
